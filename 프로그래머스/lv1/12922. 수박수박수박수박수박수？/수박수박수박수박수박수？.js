67e8182ecbec3c4b650e11195edf2aef0019f3de
심예윤 <110670796+yeyunny@users.noreply.github.com>
2023-06-01 23:39:44 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let answer = '수박';
+    const count = parseInt(n/2)
+
+    if(n%2 === 0){
+        return answer.repeat(count)
+    } return answer.repeat(count) + "수"
+    
+}
