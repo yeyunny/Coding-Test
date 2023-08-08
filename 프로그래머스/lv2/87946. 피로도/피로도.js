1c03f234f80288e89fc1be9c1de9c310f4e91d9e
심예윤 <110670796+yeyunny@users.noreply.github.com>
2023-08-08 21:52:32 +0900

    [level 2] Title: 피로도, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -0,0 +1,23 @@
+function solution(k, dungeons) {
+  let answer = -1;
+
+  const dfs = (k, dungeons, prev) => {
+      
+    for (let i = 0; i < dungeons.length; i++) {
+      const [req, use] = dungeons[i];
+        
+      if (!req || req > k) continue;
+        
+      const copy = [...dungeons].map((v) => [...v]);
+      copy[i] = [null, null];
+
+      dfs(k - use, copy, prev + 1);
+    }
+      
+    return (answer = Math.max(prev, answer));
+  };
+    
+  dfs(k, dungeons, 0);
+    
+  return answer;
+}
