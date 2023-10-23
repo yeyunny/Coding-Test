c167349bfd8178df7d3439efdb076db783729b42
심예윤 <110670796+yeyunny@users.noreply.github.com>
2023-10-23 11:29:21 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,19 +1,29 @@
-function solution(t, p) {
-    const pLen= p.length
-    const T = t.split("")
-    const arr = []
-    let count = 0
+// function solution(t, p) {
+//     const pLen= p.length
+//     const T = t.split("")
+//     const arr = []
+//     let count = 0
 
     
-    for(let i = 0; i<T.length; i++){
-        const sl = T.slice(i, pLen+i)        
-        sl.length === pLen ? arr.push(Number(sl.join(""))) : null
+//     for(let i = 0; i<T.length; i++){
+//         const sl = T.slice(i, pLen+i)        
+//         sl.length === pLen ? arr.push(Number(sl.join(""))) : null
+//     }
+    
+//     for(let i = 0; i<arr.length; i++){
+//         arr[i] <= p ? count++ : undefined
+//     }
+    
+// return count
+    
+// }
+
+
+function solution(t, p) {
+    let count = 0;
+    for(let i=0; i<=t.length-p.length; i++) {
+        let value = t.slice(i, i+p.length);
+        if(+p >= +value) count++;
     }
-    
-    for(let i = 0; i<arr.length; i++){
-        arr[i] <= p ? count++ : undefined
-    }
-    
-return count
-    
+    return count;
 }
