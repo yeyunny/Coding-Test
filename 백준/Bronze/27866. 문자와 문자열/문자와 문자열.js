036b16079f52879a9e01ef489db2770e6bdff654
YE YUN SIM <110670796+yeyunny@users.noreply.github.com>
2023-12-08 21:17:27 +0900

    [Bronze V] Title: 문자와 문자열, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(file).toString().trim().split("\n");
+//fs.readFileSync("/dev/stdin").toString().split("\n").map(Number);
+
+let sentence = input[0].split("");
+console.log(sentence[input[1] - 1]);
