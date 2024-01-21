7f146a13ea682bda6f1004fc55f4f823ad968d0b
YE YUN SIM <110670796+yeyunny@users.noreply.github.com>
2024-01-21 20:47:32 +0900

    [Bronze II] Title: 블랙잭, Time: 192 ms, Memory: 15316 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -1,18 +1,25 @@
-const input = require("fs").readFileSync(0).toString().trim().split("\n");
-const N = Number(input[0].split(" ")[0]);
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+let input = fs.readFileSync(file).toString().trim().split("\n");
+
 const M = Number(input[0].split(" ")[1]);
-const card = input[1].split(" ").map(a => +a).sort((a, b) => a - b);
+const arr = input[1]
+  .split(" ")
+  .map((a) => +a)
+  .sort((a, b) => a - b);
+
 let sum = 0;
 const CardSum = [];
-for (let i = 0; i < card.length - 2; i++) {
-    for (let j = i + 1; j < card.length - 1; j++) {
-        for (let k = j + 1; k < card.length; k++) {
-            sum = card[i] + card[j] + card[k];
-            if (M >= sum) {
-                CardSum.push(sum);
-            }
-        }
-    }
-}
 
+for (let i = 0; i < arr.length - 2; i++) {
+  for (let j = i + 1; j < arr.length - 1; j++) {
+    for (let k = j + 1; k < arr.length; k++) {
+      sum = arr[i] + arr[j] + arr[k];
+      if (M >= sum) {
+        CardSum.push(sum);
+      }
+    }
+  }
+}
 console.log(Math.max(...CardSum));
